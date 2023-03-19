d5eedd085fa03e7cb70dffe72baf578e2ea6b898
Mike <dokakeli1@gmail.com>
2023-03-19 18:36:18 +0100

    improved comments

--- increase-limit-for-post-list.php
+++ increase-limit-for-post-list.php
@@ -12,9 +12,9 @@
  */
 add_filter('graphql_connection_max_query_amount', function (int $max_amount, $source, array $args, $context, $info) {
     // Bail if the fieldName isn't avail
-    if (empty($info->fieldName)) {
-        return $max_amount;
-    }
+    // if (empty($info->fieldName)) {
+    //     return $max_amount;
+    // }
     // Bail if we're not dealing with our target fieldName
     // 	if ( 'productCategories' !== $info->fieldName ) {
     // 		return $max_amount;
